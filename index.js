b1189385f226348889081c795fa97b7af69275f7
Ben Halverson <benhalverson33@gmail.com>
2018-06-25 14:11:11 -0700

    removed request and added node-fetch

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
-// ts-check
 import request from 'request';
 // import qs from 'querystring';
 import express from 'express';
 import bodyParser from 'body-parser';
 import log from 'morgan';
+import fetch from 'node-fetch';
 // import OAuth2Server from 'express-oauth-server';
 import mongoose from 'mongoose';
 import config from './config';
@@ -11,13 +11,13 @@ import config from './config';
 
 const mongouri = config.databaseinfo.mongodbUri;
 const app = express();
-mongoose.connect(config.databaseinfo.mongodbUri, (err, res) => {
-  if (err) {
-    console.error(`Error connecting to database ${mongouri}, ${err}`);
-  } else {
-    console.log(`Database connection successful, ${mongouri}`);
-  }
-});
+// mongoose.connect(config.databaseinfo.mongodbUri, (err, res) => {
+//   if (err) {
+//     console.error(`Error connecting to database ${mongouri}, ${err}`);
+//   } else {
+//     console.log(`Database connection successful, ${mongouri}`);
+//   }
+// });
 
 // app.use(log('dev'));
 // const oauth = new OAuth2Server({
@@ -29,18 +29,40 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 // app.use(app.oauth.authorize());
 
-app.get('/request', (req, res) => {
-  request(config.oauthURL, (error, response) => {
-    console.log('error:', error); // Print the error if one occurred
-    console.log('statusCode:', response && response.statusCode); // Print the response status code if a response was received
-  });
-  res.send({ data: 'ok' });
+function encodeURL(str) {
+  return '';
+}
+app.get('/config', (req, res) => {
+  fetch(config.oauthURL)
+    .then(res => res.json())
+    .then(response => console.log(response))
+    .catch(err => console.log(err));
+  //   console.log('error:', error); // Print the error if one occurred
+  //   console.log('statusCode:', response && response.statusCode); // Print the response status code if a response was received
+  //   console.log('response', response.body);
+  // });
 });
 
-// app.get('/req', (req, res) => {
+app.get('/pricing', (req, res) => {
+  fetch('https://api.mercedes-benz.com/configurator/v1/markets/de_DE/models?bodyId=16&apikey=768e43c0-132a-489f-af85-dde91ba09822')
+    .then(res => res.json())
+    .then((response) => {
+      console.log(response[0]);
+      console.log(response[0].vehicleClass);
+      res.send('ok');
+    })
+    .catch(err => console.error(err));
+});
 
-// });
-
+app.get('/dealers', (req, res) => {
+  fetch('https://api.mercedes-benz.com/dealer/v1/dealers?radiusValue=10&radiusUnit=KM&apikey=79fed71e-ea81-4cd3-884e-96254df2e5ba')
+    .then(res => res.json())
+    .then((response) => {
+      // console.log(response);
+      const data = response;
+      console.log(data[0].dealerName);
+    });
+});
 
 app.listen(config.port, () => {
   console.log(`listing on, ${config.serverUrl}`);
